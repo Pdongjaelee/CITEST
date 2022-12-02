ba0b459ac7aac4fbe8a4d30e373076fef2f4703b
pdongjaelee <kyp4810@gmail.com>
2022-12-02 21:07:55 +0900

    add TDD

---
+++ src/test/java/MTZ/mountainz/domain/certification/controller/CertificationControllerTest.java
@@ -0,0 +1,20 @@
+package MTZ.mountainz.domain.certification.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CertificationControllerTest {
+
+    @Test
+    void certificationList() {
+
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+
+
+    }
+}
